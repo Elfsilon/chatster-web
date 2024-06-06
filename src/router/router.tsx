5abf5bdb08f496821d89cb04eb0e65936e21c2bc
maxucks <elfsilon@gmail.com>
2024-06-06 19:25:53 +0300

    - changed router to HashRouter

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { JoinChatScreen } from '../chat/screens/JoinChatScreen/JoinChat.Screen'
 import { ChatScreen } from '../chat/screens/ChatScreen/Chat.Screen'
 
 export function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<JoinChatScreen />} />
         <Route path="chat/:chatID" element={<ChatScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
